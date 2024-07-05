4e140a8e6a9611e5c4fe56722d9ecb13be98c72f
Libertocrat <libertocrat@proton.me>
2024-07-05 17:39:50 -0400

    Update: Finish backup.sh, task 12-13

--- backup.sh
+++ backup.sh
@@ -63,7 +63,9 @@ do
 done
 
 # [TASK 12]
+tar -czvf $backupFileName ${toBackup[@]}
 
 # [TASK 13]
+mv $backupFileName $destDirAbsPath
 
 # Congratulations! You completed the final project for this course!
